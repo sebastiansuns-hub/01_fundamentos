908f3cf9dc7e97205eb962f58df07356742e7ee9
sebastiansuns-hub <sunssebastian@gmail.com>
2026-04-09 17:14:39 -0500

    Delete ES7-JS-02/app/src/ejercicio18.js

--- ES7-JS-02/app/src/ejercicio18.js
+++
@@ -1,8 +0,0 @@
-Sumar números hasta llegar a 100 con while
-ObjeƟvo: repeƟr hasta cumplir condición.
-Crea un programa que vaya sumando números consecuƟvos comenzando en 1 hasta que la suma
-sea mayor o igual a 100.
-Debes mostrar:
- cada número sumado
- la suma final
- cuántos números fueron necesarios
