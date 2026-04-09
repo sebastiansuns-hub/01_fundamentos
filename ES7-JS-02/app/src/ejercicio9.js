021830d52d4cb049b645db9226e03114bce14f83
sebastiansuns-hub <sunssebastian@gmail.com>
2026-04-09 15:46:41 -0500

    Update ejercicio9.js

--- ES7-JS-02/app/src/ejercicio9.js
+++ ES7-JS-02/app/src/ejercicio9.js
@@ -1,1 +1,4 @@
-
+ Contar total de letras con reduce
+ObjeƟvo: usar reduce con strings.
+const palabras = ["hola", "mundo", "js"];
+Calcula cuántas letras hay en total sumando la longitud de cada palabra.
