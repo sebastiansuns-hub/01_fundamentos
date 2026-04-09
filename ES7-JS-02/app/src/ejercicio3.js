e01399e5c60e8aea9441bf43466b28f1bdd5431a
sebastiansuns-hub <sunssebastian@gmail.com>
2026-04-09 14:15:03 -0500

    Update ejercicio3.js

--- ES7-JS-02/app/src/ejercicio3.js
+++ ES7-JS-02/app/src/ejercicio3.js
@@ -1,1 +1,9 @@
-
+//Obtener longitudes de palabras con map
+//ObjeƟvo: usar map con strings.
+//const palabras = ["sol", "luna", "mesa", "python"];
+//Genera un arreglo con la longitud de cada palabra.
+//Ejemplo esperado:
+//[3, 4, 4, 6] 
+const palabras= ["sol", "luna", "mesa", "python"];
+const longitudes= palabras.map((word) => word.length);
+console.log(longitudes); // Output: [3, 4, 4, 6]
