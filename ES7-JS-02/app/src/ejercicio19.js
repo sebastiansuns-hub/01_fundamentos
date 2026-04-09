73fc4599774d8325a676591432ad6e2c18ad3cf1
sebastiansuns-hub <sunssebastian@gmail.com>
2026-04-09 16:07:42 -0500

    Update ejercicio19.js

--- ES7-JS-02/app/src/ejercicio19.js
+++ ES7-JS-02/app/src/ejercicio19.js
@@ -1,1 +1,7 @@
-
+Ejercicio combinado: filter + map
+ObjeƟvo: encadenar métodos.
+const numeros = [3, 8, 15, 20, 7, 12, 1, 30];
+1. Filtra solo los números mayores que 10.
+2. Luego mulƟplícalos por 2.
+Resultado esperado:
+[30, 40, 24, 60]
