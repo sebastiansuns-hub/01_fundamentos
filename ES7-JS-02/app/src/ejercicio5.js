bd374688c90cb372140b5991d49074d2b364eb9f
sebastiansuns-hub <sunssebastian@gmail.com>
2026-04-09 17:15:32 -0500

    Delete ES7-JS-02/app/src/ejercicio5.js

--- ES7-JS-02/app/src/ejercicio5.js
+++
@@ -1,6 +0,0 @@
-//Filtrar palabras largas con filter
-//ObjeƟvo: filtrar textos según su tamaño.
-//const palabras = ["casa", "vidrio", "sol", "camara", "luz"];
-//Obtén solo las palabras que tengan más de 5 letras
-const palabras=["casa","vidrio","sol","camara","luz"].filter ((word) => word.length > 5);
-console.log(palabras); 
