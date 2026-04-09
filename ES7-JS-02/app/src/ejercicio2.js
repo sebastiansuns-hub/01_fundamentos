2b753dd979b2d9d1a2c12b154bbeaf6d4014f5b8
sebastiansuns-hub <sunssebastian@gmail.com>
2026-04-09 14:12:49 -0500

    Update ejercicio2.js

--- ES7-JS-02/app/src/ejercicio2.js
+++ ES7-JS-02/app/src/ejercicio2.js
@@ -1,1 +1,7 @@
-
+//ConverƟr nombres a mayúsculas con map
+//ObjeƟvo: transformar cadenas de texto.
+//const nombres = ["juan", "luis", "pepito", "pedro"];
+//Crea un nuevo arreglo con todos los nombres en mayúsculas. 
+const values = ["juan", "luis", "pepito", "pedro"];
+const mayusculas = values.map((name) => name.toUpperCase());
+console.log(mayusculas); // Output: ["JUAN", "LUIS", "PEPITO", "PEDRO"]
