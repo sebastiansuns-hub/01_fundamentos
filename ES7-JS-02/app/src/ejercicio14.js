9080f01779902b047a5f2f1a3251b13cd4438a1d
sebastiansuns-hub <sunssebastian@gmail.com>
2026-04-09 16:05:28 -0500

    Update ejercicio14.js

--- ES7-JS-02/app/src/ejercicio14.js
+++ ES7-JS-02/app/src/ejercicio14.js
@@ -1,9 +1,11 @@
-Ordenar productos por precio con sort
-ObjeƟvo: ordenar objetos.
-const productos = [
- { nombre: "Teclado", precio: 120000 },
- { nombre: "Mouse", precio: 50000 },
- { nombre: "Monitor", precio: 800000 },
- { nombre: "USB", precio: 30000 }
-];
-Ordénalos del más barato al más caro.
+ Menú de día de la semana con switch
+ObjeƟvo: tomar decisiones con múlƟples casos.
+Solicita un número del 1 al 7 e imprime el día de la semana correspondiente:
+ 1 → Lunes
+ 2 → Martes
+ 3 → Miércoles
+ 4 → Jueves
+ 5 → Viernes
+ 6 → Sábado
+ 7 → Domingo
+Si el número no está entre 1 y 7, mostrar: "Día no válido".
