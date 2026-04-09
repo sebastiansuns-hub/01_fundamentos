6ac47c54844882b1fdf470939bd1bf01c410f40f
sebastiansuns-hub <sunssebastian@gmail.com>
2026-04-09 14:11:00 -0500

    Update ejercicio1.js

--- ES7-JS-02/app/src/ejercicio1.js
+++ ES7-JS-02/app/src/ejercicio1.js
@@ -1,1 +1,10 @@
-
+//Duplicar números con map
+//ObjeƟvo: pracƟcar transformación de arreglos.
+//Dado el arreglo:
+//const numeros = [2, 4, 6, 8, 10];
+//Crea un nuevo arreglo donde cada número esté mulƟplicado por 2.
+//Salida esperada:
+//[4, 8, 12, 16, 20] 
+const values = [2, 4, 6, 8, 10];
+const doubled = values.map((x) => x * 2);
+console.log(doubled); // Output: [4, 8, 12, 16, 20]
