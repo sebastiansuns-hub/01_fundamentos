9d92b49ebb2eea84e25eee0aec22c61e64cfc22b
sebastiansuns-hub <sunssebastian@gmail.com>
2026-04-09 16:11:48 -0500

    Add exercise for integrating array methods
    Add integrative exercise using filter, sort, map, and reduce methods with sales data.

--- ES7-JS-02/app/src/ejercicio20.js
+++ ES7-JS-02/app/src/ejercicio20.js
@@ -1,1 +1,8 @@
-
+Ejercicio integrador: filter + sort + map + reduce
+ObjeƟvo: integrar varios métodos en un solo problema.
+const ventas = [
+ { producto: "Mouse", canƟdad: 3, precio: 50000 },
+ { producto: "Teclado", canƟdad: 2, precio: 120000 },
+ { producto: "Monitor", canƟdad: 1, precio: 800000 },
+ { producto: "USB", canƟdad: 5, precio: 30000 }
+];
