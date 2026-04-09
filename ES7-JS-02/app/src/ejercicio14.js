a7efe07559d8a539ce75c159e7ee77c3749f405a
sebastiansuns-hub <sunssebastian@gmail.com>
2026-04-09 16:05:02 -0500

    Update ejercicio14.js

--- ES7-JS-02/app/src/ejercicio14.js
+++ ES7-JS-02/app/src/ejercicio14.js
@@ -1,1 +1,9 @@
-
+Ordenar productos por precio con sort
+ObjeƟvo: ordenar objetos.
+const productos = [
+ { nombre: "Teclado", precio: 120000 },
+ { nombre: "Mouse", precio: 50000 },
+ { nombre: "Monitor", precio: 800000 },
+ { nombre: "USB", precio: 30000 }
+];
+Ordénalos del más barato al más caro.
